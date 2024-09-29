14a0e544f006c7e1e139c174be8d775ca009866d
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2024-09-29 22:38:24 +0200

    chore: 1st lint

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,18 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-import '@tamagui/core/reset.css';
-import { TamaguiProvider } from 'tamagui'
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import "@tamagui/core/reset.css";
+import { TamaguiProvider } from "tamagui";
 
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { tamaguiConfig } from '@/tamagui.config';
-
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { tamaguiConfig } from "@/tamagui.config";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -18,8 +20,8 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
   });
 
   useEffect(() => {
@@ -34,7 +36,7 @@ export default function RootLayout() {
 
   return (
     <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,10 +1,10 @@
-import { config } from '@tamagui/config/v3'
-import { createTamagui } from 'tamagui'
+import { config } from "@tamagui/config/v3";
+import { createTamagui } from "tamagui";
 
-export const tamaguiConfig = createTamagui(config)
+export const tamaguiConfig = createTamagui(config);
 
-export type Conf = typeof tamaguiConfig
+export type Conf = typeof tamaguiConfig;
 
-declare module 'tamagui' {
+declare module "tamagui" {
   interface TamaguiCustomConfig extends Conf {}
 }
