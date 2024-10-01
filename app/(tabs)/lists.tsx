7bd71afb739daf0d4763e5367b173f1647d83d15
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2024-10-01 14:52:14 +0200

    feat: update display

--- app/(tabs)/lists.tsx
+++ app/(tabs)/lists.tsx
@@ -1,47 +1,28 @@
-import { Image, StyleSheet } from "react-native";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { StyleSheet } from "react-native";
 import { useLiveQuery } from "drizzle-orm/expo-sqlite";
 import { db } from "@/db";
 import { lists } from "@/db/schema";
-import { Card, H3, ScrollView, YStack } from "tamagui";
+import { ScrollView, YStack } from "tamagui";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
-import { Button } from "tamagui";
+import Container from "@/components/Container";
+import ListCardItem from "@/components/ListCard";
 
 export default function ListsScreen() {
   const { data } = useLiveQuery(db.select().from(lists));
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
+    <Container>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Mes Listes</ThemedText>
       </ThemedView>
-      <ScrollView
-        maxHeight={400}
-        width="100%"
-        // backgroundColor="$background"
-        // padding="$4"
-        borderRadius="$4"
-      >
-        <YStack gap={16}>
+      <ScrollView maxHeight={800} width="100%" borderRadius="$4">
+        <YStack gap="$6">
           {data.map((list) => (
-            <Card key={list.id} elevate size="$4" bordered>
-              <Card.Header padded>
-                <H3>{list.name}</H3>
-                <Button borderRadius="$10">Voir plus</Button>
-              </Card.Header>
-            </Card>
+            <ListCardItem key={list.id} list={list} />
           ))}
         </YStack>
       </ScrollView>
-    </ParallaxScrollView>
+    </Container>
   );
 }
 
@@ -50,12 +31,5 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     gap: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
   },
 });
