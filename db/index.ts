7046b22e091d1439bbe68b083aca1f7decbea60d
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2024-10-10 12:33:05 +0200

    chore: new db instance

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { openDatabaseSync } from "expo-sqlite/next";
 
-const expo = openDatabaseSync("app.db", { enableChangeListener: true });
+const expo = openDatabaseSync("app2.db", { enableChangeListener: true });
 export const db = drizzle(expo);
