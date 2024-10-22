2d028043f1818d270899fc25c82a73e20e22509c
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2024-10-22 15:57:00 +0200

    feat: add column to db

--- db/schema.ts
+++ db/schema.ts
@@ -3,6 +3,7 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const lists = sqliteTable("lists", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name", { length: 50 }).notNull().unique(),
+  isGlobal: integer("is_global", { mode: "boolean" }).notNull().default(false),
   items: text("items")
     .notNull()
     .$defaultFn(() => ""),
--- drizzle/0000_fine_slyde.sql
+++ drizzle/0000_fine_slyde.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `lists` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text(50) NOT NULL,
-	`items` text DEFAULT (json_array()) NOT NULL
+	`is_global` integer DEFAULT false NOT NULL,
+	`items` text NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `lists_name_unique` ON `lists` (`name`);
