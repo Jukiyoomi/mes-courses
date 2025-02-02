df2660a6410ae6804de0772adde2c9c0a80c9798
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2025-02-02 19:47:51 +0100

    refactor: move files to db folder

---
+++ db/migrations/0001_majestic_spitfire.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `items` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text(50) NOT NULL,
+	`taken` integer DEFAULT false NOT NULL
+);
+--> statement-breakpoint
+/*
+ SQLite does not support "Drop default from column" out of the box, we do not generate automatic migration for that, so it has to be done manually
+ Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
+                  https://www.sqlite.org/lang_altertable.html
+                  https://stackoverflow.com/questions/2083543/modify-a-columns-type-in-sqlite3
+
+ Due to that we don't generate migration automatically and it has to be done manually
+*/
